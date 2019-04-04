42068d9e55bbff90b53789190ceae2b633860dd4
Pablo Ortiz Barra <portiz@dgtm.cl>
2019-04-04 16:37:19 -0300

    equipo cargando desde firebase realtime

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,20 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { EquipoPagina } from '../interfaces/equipo-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
   info: InfoPagina = {};
+  equipo: EquipoPagina = {};
   cargada = false;
+  
   constructor(private http: HttpClient) {
-    // console.log('servicio de infopagina cargada');
-    // traer datos de json
+      this.cargarInfo();
+      this.cargarEquipo();
+  }
+
+  private cargarInfo() {
+
     this.http.get('assets/data/data-pagina.json').subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
       // console.log(resp);
+      // console.log(resp['email']);
+    });
+
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-349c8.firebaseio.com/equipo.json').subscribe((resp: EquipoPagina) => {
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(resp);
       // console.log(resp['email']);
     });
   }
